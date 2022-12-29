5ed76f93cb2dbacaff5c7a4513b3adfb610008da
Igor Ramos <igor.ramos@live.com>
2022-12-29 15:32:31 -0300

    Finalizando a sidebar

--- ignite/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ ignite/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
   return (
@@ -9,12 +10,14 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img src="https://github.com/iramosg.png" />
         <strong>Igor Ramos</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
