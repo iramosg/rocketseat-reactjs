d72bf75b1c16a82fa6222bf839698edcaf3f397a
Igor Ramos <igor.ramos@live.com>
2022-12-31 17:38:52 -0300

    Adaptando a aplicação para TS

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,85 @@
+import { Header } from "./components/Header"
+import { Content, Post, PostProps } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
+
+import './global.css'
+import styles from './App.module.css'
+
+
+
+interface Post extends PostProps {
+  id: number
+  content: Content[]
+}
+
+
+const posts: Post[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/iramosg.png",
+      name: "Igor Ramos",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-31 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/iramosg.png",
+      name: "Teste Usuário",
+      role: "UX Designer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-01-01 20:00:00')
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/iramosg.png",
+      name: "Maria Teste",
+      role: "QA"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-02-01 20:00:00')
+  },
+]
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          { posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          }) }
+        </main>
+      </div>
+    </div>
+   
+  )
+}
+
---
+++ 01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+  content: string
+  onDeleteComment: (commentToDelete: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0) 
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(state => {
+      return state + 1
+    })
+  }
+
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/iramosg.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Igor Ramos</strong>
+              <time dateTime="2022-29-12 15:00:00" title="29 de Dezembro às 15 horas">Cerca de há 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+          
+        </footer>
+      </div>
+
+    </div>
+  )
+}
