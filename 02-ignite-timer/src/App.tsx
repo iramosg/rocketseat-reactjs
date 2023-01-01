c4278d1c6c3beaea43d6e34260043683de046031
Igor Ramos <igor.ramos@live.com>
2023-01-01 12:48:10 -0300

    Início dos estilos globais

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 
 import { Button } from "./components/Button"
+import { GlobalStyle } from "./styles/global"
 
 export function App() {
 
@@ -12,6 +13,8 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
    </ThemeProvider>
   )
 }
